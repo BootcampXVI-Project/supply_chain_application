b45da7b89b5da5b40e50def58e0623a1b83cdfee
nero <tapnguyen@gmail.com>
2023-05-18 13:11:30 +0700

    CRUD API order

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -328,7 +328,84 @@ const ProductController = {
 		// 		error: error
 		// 	});
 		// }
-	}
+	},
+
+	createOrder: async (req: Request, res: Response) => {
+		try {
+			const userId = String(req.body.userId);
+			const orderObj = req.body.orderObj;
+			const userObj = await getUserByUserId(userId);
+
+			console.log(userId);
+			console.log(userObj);
+
+			await submitTransaction("CreateOrder", userObj, orderObj);
+
+			return res.json({
+				data: null,
+				message: "successfully",
+				error: null
+			});
+		} catch (error) {
+			return res.json({
+				data: null,
+				message: "failed",
+				error: error
+			});
+		}
+	},
+
+	updateOrder: async (req: Request, res: Response) => {
+		try {
+			const userId = String(req.body.userId);
+			const orderObj = req.body.orderObj;
+			const userObj = await getUserByUserId(userId);
+
+			console.log(userId);
+			console.log(userObj);
+
+			await submitTransaction("UpdateOrder", userObj, orderObj);
+
+			return res.json({
+				data: null,
+				message: "successfully",
+				error: null
+			});
+		} catch (error) {
+			return res.json({
+				data: null,
+				message: "failed",
+				error: error
+			});
+		}
+	},
+
+	finishOrder: async (req: Request, res: Response) => {
+		try {
+			const userId = String(req.body.userId);
+			const orderObj = req.body.orderObj;
+			const userObj = await getUserByUserId(userId);
+
+			console.log(userId);
+			console.log(userObj);
+
+			await submitTransaction("FinishOrder", userObj, orderObj);
+
+			return res.json({
+				data: null,
+				message: "successfully",
+				error: null
+			});
+		} catch (error) {
+			return res.json({
+				data: null,
+				message: "failed",
+				error: error
+			});
+		}
+	},
+
+
 };
 
 export default ProductController;
