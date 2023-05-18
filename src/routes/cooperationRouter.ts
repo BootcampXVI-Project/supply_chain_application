d3805279cbf3bbafe9a4d765706e8373c0e7531b
giahung2810 <rynthanthien@gmail.com>
2023-05-18 11:18:36 +0700

    update

---
+++ src/routes/cooperationRouter.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import ProductController from "../controllers/CooperationController";
+
+const router = express.Router();
+
+// router.get("/all", CooperationController.getAllCooperations);
+// router.get("/", CooperationController.getCooperation);
+// router.post("/cultivate", CooperationController.cultivateCooperation);
+
+export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Express } from "express";
 import productRouter from "./productRouter";
 import userRouter from "./userRouter";
-
+import cooperationRouter from "./cooperationRouter";
 function routing(app: Express) {
 	app.use("/product", productRouter);
 	app.use("/user", userRouter);
+	app.use("/cooperation", cooperationRouter);
 
 	app.use("*", (req, res, next) => {
 		res.status(404).json({
