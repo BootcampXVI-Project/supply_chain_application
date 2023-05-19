6cd8de95c53d3266b92b8edc8fa80645f9c6d2cc
goprodeptrai01 <tapnguyen01@gmail.com>
2023-05-19 17:03:52 +0700

    init, create service upload image

---
+++ src/services/crudDatabase/image.ts
@@ -0,0 +1,37 @@
+import admin from "firebase-admin";
+
+// Path to your Firebase service account key file
+const serviceAccount = require("../../../../SeniorProject/supply-chain-9ea64-firebase-adminsdk-hz2j8-94d0fecb0a.json");
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	storageBucket: "gs://supply-chain-9ea64.appspot.com"
+	// Add any other configuration options here
+});
+
+const bucket = admin.storage().bucket();
+
+export default class ImageService {
+	static async upload(imagePath: string, nameImage: string) {
+		try {
+			const currentDate = new Date();
+			const expirationDate = new Date(
+				currentDate.getTime() + 365 * 24 * 60 * 60 * 1000
+			);
+			console.log(imagePath, nameImage);
+
+			const response = await bucket.upload(imagePath, {
+				destination: nameImage
+			});
+
+			const url = await response[0].getSignedUrl({
+				action: "read",
+				expires: expirationDate // Adjust the expiration date as desired
+			});
+			console.log("Image uploaded successfully.");
+			return url;
+		} catch (error) {
+			console.error("Error uploading image:", error);
+		}
+	}
+}
