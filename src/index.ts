4bdc80d26d00b4c3e46c3bff93f4d632368d0b86
tpn <tapnguyen01@gmail.com>
2023-06-03 15:36:21 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,11 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpecs));
 
 // Middlewares
 app.use(cors());
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+  });
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
